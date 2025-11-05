e0fa5d9de245f204b6f76b49db41c392ee1fc8c8
Miteny <124059014+teny99@users.noreply.github.com>
2025-11-05 23:53:52 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { appDir: true }
-}
-module.exports = nextConfig
+  // enlève l'objet `experimental` ou au minimum la clé `appDir`
+  // garde ici le reste de ta config si tu as d'autres clés
+};
+
+module.exports = nextConfig;
